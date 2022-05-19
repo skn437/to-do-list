8f9742297ac09b132b761ef3fe0c777a8572ce78
SKN <hellno@gmail.com>
2022-05-20 01:06:26 +0600

    Improved onClick or onSubmit events

--- src/CreateToDo.js
+++ src/CreateToDo.js
@@ -7,37 +7,35 @@ const CreateToDo = () => {
     const [todo, setToDo] = useState("");
     const [isLoading, setIsLoading] = useState(false);
 
-    const handleAdd = e => {
-
-        e.preventDefault();
-
-        setIsLoading(true);
-
-        const colRef = collection(db, "todos");
-
-        addDoc(colRef, {
-
-           content: todo,
-           createdAt: serverTimestamp()
-
-        }).then(() => {
-
-            console.log(`To-Do Added`);
-            setIsLoading(false);
-
-        }).catch(err => {
-
-            console.log(err.message);
-
-        });
-
-    };
-
     return (
 
         <div id="create-list">
 
-            <form id="create-form" onSubmit={handleAdd}>
+            <form id="create-form" onSubmit={e => {
+
+                e.preventDefault();
+
+                setIsLoading(true);
+
+                const colRef = collection(db, "todos");
+
+                addDoc(colRef, {
+
+                    content: todo,
+                    createdAt: serverTimestamp()
+
+                }).then(() => {
+
+                    console.log(`To-Do Added`);
+                    setIsLoading(false);
+
+                }).catch(err => {
+
+                    console.log(err.message);
+
+                });
+
+            }}>
 
                 <label htmlFor="create-to-do">Add New To-Do: </label><br/><br/>
                 <input type="text" id="create-to-do" placeholder="type something...." value={todo} onChange={e => setToDo(e.target.value)}></input>
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -3,29 +3,27 @@ import { db } from "./Firebase";
 
 const ToDoList = ({todos}) => {
 
-    const handleDelete = id => {
-
-        const docRef = doc(db, "todos", id);
-
-        deleteDoc(docRef).then(() => {
-
-            console.log(`Item deleted of `, id);
-    
-        }).catch(err => {
-    
-            console.log(err.message);
-    
-        });
-
-    };
-
     return (
 
         <div id="todos">
 
             {todos.length ? todos.map(todo => (
 
-                    <div key={todo.id} className="list" onClick={() => handleDelete(todo.id)}>
+                    <div key={todo.id} className="list" onClick={() => {
+
+                        const docRef = doc(db, "todos", todo.id);
+
+                        deleteDoc(docRef).then(() => {
+                
+                            console.log(`Item deleted of `, todo.id);
+                    
+                        }).catch(err => {
+                    
+                            console.log(err.message);
+                    
+                        });
+
+                    }}>
 
                         <p>{todo.content}</p>
 
